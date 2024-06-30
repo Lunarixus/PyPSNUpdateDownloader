3f91e96c1531d161bb20a4d28204f98d02d485fd
Lunarixus <Nathan@lunarixus.dev>
2024-06-30 03:51:15 +0100

    PyPSNUpdateDownloader: Initial v1.0.0
    Signed-off-by: Lunarixus <Nathan@lunarixus.dev>

---
+++ create-version-file.py
@@ -0,0 +1,13 @@
+import pyinstaller_versionfile
+
+pyinstaller_versionfile.create_versionfile(
+    output_file="version.txt",
+    version="1.0.0.0",
+    company_name="Lunarixus",
+    file_description="Download PSN Update Packages",
+    internal_name="PyPSNUpdateDownloader",
+    legal_copyright="© 2024 Lunarixus. All rights reserved.",
+    original_filename="PyPSNUpdateDownloader.exe",
+    product_name="PyPSNUpdateDownloader",
+    translations=[0, 1200]
+)
